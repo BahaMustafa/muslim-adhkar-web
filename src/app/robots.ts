6192da6bde0edb3c2fa91af55ab583269231b2ff
BahaMustafa <bhaatobase93@gmail.com>
2026-01-03 01:02:06 -0800

    chore: add robots.txt pointing to both sitemaps

---
+++ src/app/robots.ts
@@ -0,0 +1,17 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+    const baseUrl = 'https://muslim-adhkar.com'
+
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: ['/private/', '/api/'],
+        },
+        sitemap: [
+            `${baseUrl}/sitemap.xml`,
+            `${baseUrl}/sitemap-quran.xml`,
+        ],
+    }
+}
